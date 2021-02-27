4ffa5af06256329168e5feeecc7b976a8809b623
abhirukth <abhirukthkc@gmail.com>
2021-02-27 11:11:13 +0530

    Chat application pushed

---
+++ app/src/main/java/com/example/ko_net/SIGNUP.java
@@ -0,0 +1,14 @@
+package com.example.ko_net;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SIGNUP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_s_i_g_n_u_p);
+    }
+}
